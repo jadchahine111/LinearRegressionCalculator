9cb03d7a798afa4cca16964fe01a2bcc32a92dcf
= <=>
2024-06-16 05:00:36 +0300

    Interface & Page Model Implementation

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using LinearRegressionCalculator.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -7,6 +8,13 @@ namespace LinearRegressionCalculator.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
+        public List<Point> Coordinates { get; set; } = new List<Point>();
+        public Line Line { get; set; }
+        
+        public LinearRegression linearRegression { get; set; }
+
+
+
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
@@ -15,6 +23,21 @@ namespace LinearRegressionCalculator.Pages
         public void OnGet()
         {
 
+        }
+
+        public IActionResult OnPost(List<Point> coordinates)
+        {
+            Coordinates = coordinates;
+
+            // Add each coordinate as a point to the Line
+            foreach (var point in coordinates)
+            {
+                Line.AddPoint(point);
+            }
+            linearRegression.Line = Line;
+            double a0 = linearRegression.B();
+            double a1 = linearRegression.A();
+            return Page();
         }
     }
 }
---
+++ Pages/Team.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LinearRegressionCalculator.Pages
+{
+    public class TeamModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
